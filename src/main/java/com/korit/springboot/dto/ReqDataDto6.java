653eff187c434f94fe41dec9e7fc5a721094044b
jiny-01 <allie7019@naver.com>
2025-12-04 02:49:47 +0900

    DTO 패키지 생성

---
+++ src/main/java/com/korit/springboot/dto/ReqDataDto6.java
@@ -0,0 +1,13 @@
+package com.korit.springboot.dto;
+
+import lombok.Data;
+
+@Data
+public class ReqDataDto6 {
+    private String name;
+    private int age;
+    private String address;
+    private String phone;
+
+
+}
---
+++ src/main/java/com/korit/springboot/dto/ReqJsonDto2.java
@@ -0,0 +1,8 @@
+package com.korit.springboot.dto;
+
+public class ReqJsonDto2 {
+    private String name;
+    private int age;
+
+
+}
